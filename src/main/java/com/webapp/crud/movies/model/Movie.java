fefbe4c1af8e73672906b27d06f76ea0f2021c53
agarazel <mickael.saraga@outlook.com>
2019-04-10 02:10:16 +0200

    Initial models creation

---
+++ src/main/java/com/webapp/crud/movies/model/Movie.java
@@ -0,0 +1,61 @@
+package com.webapp.crud.movies.model;
+
+import java.time.LocalDateTime;
+
+public class Movie {
+  
+  private String name;
+  private String director;
+  private String writer;
+  private String producer;
+  private MovieGenre genre;
+  private LocalDateTime releaseDate;
+  
+  public String getName() {
+    return name;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public String getDirector() {
+    return director;
+  }
+  
+  public void setDirector(String director) {
+    this.director = director;
+  }
+  
+  public String getWriter() {
+    return writer;
+  }
+  
+  public void setWriter(String writer) {
+    this.writer = writer;
+  }
+  
+  public String getProducer() {
+    return producer;
+  }
+  
+  public void setProducer(String producer) {
+    this.producer = producer;
+  }
+  
+  public MovieGenre getGenre() {
+    return genre;
+  }
+  
+  public void setGenre(MovieGenre genre) {
+    this.genre = genre;
+  }
+  
+  public LocalDateTime getReleaseDate() {
+    return releaseDate;
+  }
+  
+  public void setReleaseDate(LocalDateTime releaseDate) {
+    this.releaseDate = releaseDate;
+  }
+}
---
+++ src/main/java/com/webapp/crud/movies/model/MovieGenre.java
@@ -0,0 +1,18 @@
+package com.webapp.crud.movies.model;
+
+public enum MovieGenre {
+  
+  ACTION,
+  ADVENTURE,
+  THRILLER,
+  FILM_NOIR,
+  FANTASY,
+  SCIENCE_FICTION,
+  WESTERN,
+  COMEDY,
+  HISTORY,
+  DRAMA,
+  ROMANCE,
+  BIOPIC
+  
+}
