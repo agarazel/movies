c9c6dfcbe7bf93567758bc8270993d3887dd6f2c
agarazel <mickael.saraga@outlook.com>
2019-04-19 14:24:01 +0200

    WIP

--- src/main/java/com/webapp/crud/movies/Start.java
+++ src/main/java/com/webapp/crud/movies/Start.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@ComponentScan("com.webapp.crud.movies.repository")
 public class Start {
   
   public static void main(String[] args) {
--- src/main/java/com/webapp/crud/movies/controller/MainController.java
+++ src/main/java/com/webapp/crud/movies/controller/MainController.java
@@ -3,23 +3,30 @@ package com.webapp.crud.movies.controller;
 import com.webapp.crud.movies.model.Movie;
 import com.webapp.crud.movies.service.IMoviesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class MainController {
+  
+  @Autowired
+  private IMoviesService moviesService;
   
   public MainController(IMoviesService moviesService) {
     this.moviesService = moviesService;
   }
   
-  @Autowired
-  private IMoviesService moviesService;
+  @GetMapping(path = "/start")
+  public String getIndex() {
+    return "Start here";
+  }
   
   @GetMapping(path = "/movies")
-  public List<Movie> getMovieList() {
+  public List<Movie> getMoviesList() {
     return moviesService.getAllMovies();
   }
   
